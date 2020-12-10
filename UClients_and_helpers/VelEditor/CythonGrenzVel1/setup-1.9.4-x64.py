ea7ad2b509a4bcb70a230d6d25253c681c69af20
martburg <burger@wiredworks.com>
2020-12-10 11:19:19 +0100

    GernzVel cyton

---
+++ UClients_and_helpers/VelEditor/CythonGrenzVel1/setup-1.9.4-x64.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
+
+
+#from distutils.core import setup
+from Cython.Build import cythonize
+import numpy as np
+#import scipy
+
+setup(
+  name = 'GrenzVel',
+  include_dirs = [np.get_include()],
+  ext_modules = cythonize("GrenzVel.pyx",annotate=True),
+)
